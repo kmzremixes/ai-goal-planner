d9be73832f82e90434438f0b1d0bde3611258693
เพลินใจ ติดฝน <artskmz6@gmail.com>
2025-07-29 00:17:59 +0000

    AIzaSyB2NUml-SwnLIzrMYsW7VkDRxjJ-sW8zyo

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI('AIzaSyB2NUml-SwnLIzrMYsW7VkDRxjJ-sW8zyo')],
   model: 'googleai/gemini-2.0-flash',
 });
